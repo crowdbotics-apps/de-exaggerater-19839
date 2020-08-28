d4aa2f9227b810ab9218eec75e8b7db33d48aa3f
Crowdbotics <team@crowdbotics.com>
2020-08-28 07:16:45 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/de_exaggerater_19839/MainActivity.java
@@ -0,0 +1,15 @@
+package com.de_exaggerater_19839;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "de_exaggerater_19839";
+    }
+}
